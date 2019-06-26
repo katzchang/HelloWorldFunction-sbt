4c64a3e32d606e51ac53b3310e99abeafb66ed01
katzchang <katzchang@gmail.com>
2019-06-26 15:32:36 +0900

    let it better

--- build.sbt
+++ build.sbt
@@ -9,20 +9,10 @@ scalaVersion := "2.12.8"
 javacOptions ++= Seq("-source", "1.8", "-target", "1.8", "-Xlint")
 scalacOptions := Seq("-target:jvm-1.8")
 
-//initialize := {
-//  val _ = initialize.value
-//  if (sys.props("java.specification.version") != "1.8")
-//    sys.error("Java 8 is required for this project.")
-//}
-
 resolvers += Classpaths.typesafeReleases
 
-val aws_lambda_java_core = "com.amazonaws" % "aws-lambda-java-core" % "1.2.0"
-val junit = "junit" % "junit" % "4.12" % "test"
+libraryDependencies += "com.amazonaws" % "aws-lambda-java-core" % "1.2.0"
+libraryDependencies += "com.amazonaws" % "aws-lambda-java-events" % "2.2.6"
 
-libraryDependencies ++= Seq(
-  aws_lambda_java_core,
-
-  junit
-)
+libraryDependencies += "junit" % "junit" % "4.12" % "test"
 
--- src/main/scala/helloworld/scala/App.scala
+++ src/main/scala/helloworld/scala/App.scala
@@ -5,19 +5,20 @@ import java.net.URL
 import java.util
 import java.util.stream.Collectors
 
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 
 /**
   * Handler for requests to Lambda function.
   */
-class App extends RequestHandler[AnyRef, AnyRef] {
-  override def handleRequest(input: AnyRef, context: Context): AnyRef = {
+class App extends RequestHandler[APIGatewayProxyRequestEvent, GatewayResponse] {
+  override def handleRequest(input: APIGatewayProxyRequestEvent, context: Context): GatewayResponse = {
     val headers = new util.HashMap[String, String]
     headers.put("Content-Type", "application/json")
     headers.put("X-Custom-Header", "application/json")
     try {
       val pageContents = this.getPageContents("https://checkip.amazonaws.com")
-      val output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents)
+      val output = s"""{ "message": "hello world", "location": "$pageContents", "input": "$input" }"""
       GatewayResponse(output, headers, 200)
     } catch {
       case _: IOException =>
@@ -27,11 +28,12 @@ class App extends RequestHandler[AnyRef, AnyRef] {
 
   private def getPageContents(address: String) = {
     val url = new URL(address)
+    val br = new BufferedReader(new InputStreamReader(url.openStream))
+
     try {
-      val br = new BufferedReader(new InputStreamReader(url.openStream))
-      try
-        br.lines.collect(Collectors.joining(System.lineSeparator))
-      finally if (br != null) br.close()
+      br.lines.collect(Collectors.joining(System.lineSeparator))
+    } finally {
+      if (br != null) br.close()
     }
   }
 }
